4e6b3a402bb28242a784e61d70e4521f6fb97f2a
Michael Simmons <MSimmons@convercent.com>
2017-07-24 15:35:42 -0600

    Updated WebServer Configuration to include IIS features

---
+++ DSC/WebServer.ps1
@@ -0,0 +1,17 @@
+﻿configuration WebServer {
+    File DeploymentAnswers {
+        DestinationPath = "C:\Environment\DeploymentAnswers.xml"
+        Ensure = "Present"
+        Type = "File"
+        Contents = "Starting Somewhere"
+    }
+
+    WindowsFeature Web-WebServer {
+        Name = "Web-WebServer"
+        Ensure = 'Present'
+        IncludeAllSubFeature = $true
+    }
+
+    
+
+}
